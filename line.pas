ce64b8f991b41f3354219e45ae3480e8a125e4cb
ArmedToken19 <sevadon4@gmail.com>
2019-06-15 13:06:46 +0300

    Create line.pas
    Add line.pas

---
+++ line.pas
@@ -0,0 +1,18 @@
+﻿program summ;
+
+var
+  num, rem, sum: integer;
+  
+begin
+  num := 1234;
+  sum := 0;
+  
+  while num > 0 do begin
+    rem := num mod 10;
+    num := num div 10;
+    sum := sum + rem;
+  end;
+  
+  writeln(sum)
+end.
+
