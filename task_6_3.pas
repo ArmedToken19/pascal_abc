0c8a9f00cc3a5c3398d8f7ca4ca80050c5961291
ArmedToken19 <sevadon4@gmail.com>
2019-06-16 16:50:25 +0300

    Create task_6_3.pas
    Окулов.  Глава 6, задача 3.

---
+++ task_6_3.pas
@@ -0,0 +1,18 @@
+﻿program task_6_3;
+  var
+    i, n, a, s, r, res: integer;
+begin
+  readln(n);
+  for i := 100 to 999 do
+  begin
+    s := i;
+    r := 0;
+    while s > 0 do begin
+      a := s mod 10;
+      s := s div 10;
+      r := r + a;
+    end;
+    if (r = n) then res := res + 1;
+  end;
+  writeln(res, ' раз');
+end.
