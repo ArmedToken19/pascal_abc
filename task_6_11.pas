d7aa2698d2d680148d67902918e4d8585f9d53cd
ArmedToken19 <sevadon4@gmail.com>
2019-06-21 02:03:34 +0300

    Create task_6_11.pas

---
+++ task_6_11.pas
@@ -0,0 +1,13 @@
+﻿program task_6_11;
+
+var
+  i, r, x: integer;
+  
+begin
+  for i := 21 to 99 do begin
+    x := i mod 10;
+    if (i mod 3 = 0) and ((x = 2) or (x = 4) or (x = 8)) then 
+    r := r + i
+  end;
+  writeln(r);
+end.
