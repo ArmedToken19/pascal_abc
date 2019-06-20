026befe440c7773f1e52a999d7c004efdf4d8cf8
ArmedToken19 <sevadon4@gmail.com>
2019-06-20 12:38:37 +0300

    Create Task_6_8.pas

---
+++ Task_6_8.pas
@@ -0,0 +1,10 @@
+﻿program task_6_8;
+  var
+    i, s, t: integer;
+begin
+  for i := 1000 to 9999 do begin;
+    if (i mod 134 = 111) and (i mod 133 = 125) then
+      writeln(i, ' подходит')
+  end;
+end.
+    
