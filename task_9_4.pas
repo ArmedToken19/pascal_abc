bddfff2a8e4cbc78bf62627f6f12b7267a3f27e2
ArmedToken19 <sevadon4@gmail.com>
2019-08-02 14:37:41 +0500

    Create task_9_4.pas

---
+++ task_9_4.pas
@@ -0,0 +1,14 @@
+﻿program task_9_4;
+var
+  i, j, n: integer;
+begin
+  write('n=');
+  readln(n);
+  for i:=1 to n do
+  begin
+    write(i);
+    for j:=1 to i do
+        if i mod j = 0 then write('+');
+        writeln;
+    end;
+end.
