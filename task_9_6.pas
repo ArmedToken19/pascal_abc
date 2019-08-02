2f4c7c85184fd4fb7c0dde8b5396520b4e62bac9
ArmedToken19 <sevadon4@gmail.com>
2019-08-02 14:45:16 +0500

    Create task_9_6.pas

---
+++ task_9_6.pas
@@ -0,0 +1,19 @@
+﻿program task_9_6;
+var
+  I, j, s, m, d: integer;
+begin
+  m := 0;
+  d := 0;
+  for i := 1 to 10000 do
+  begin
+    s := 0;
+    for j := 1 to i do
+    If i mod j = 0 then s := s + j;
+    If s > m then
+    begin
+    m := s;
+    d := I;
+    end;
+  end;
+  writeln ('Число - ', d , ' ' , 'сумма делителей - ', m);
+end.
